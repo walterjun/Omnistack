cec61af58c0ca2a1c2f5c6e17f66d8309850b038
Walter junior <waltergsjunior@gmail.com>
2021-07-17 17:15:08 -0300

    Update proxy.js

--- src/proxy.js
+++ src/proxy.js
@@ -3,7 +3,7 @@ const tunnel = require('tunnel');
 const proxy = {
     host: '172.19.100.252',
     port: 8080,
-    proxyAuth: 'wjunior:$N!per5303480'
+    proxyAuth: ''
   };
 const tunnelProxy = tunnel.httpsOverHttp({
     proxy: proxy
